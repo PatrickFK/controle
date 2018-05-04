3b6e6eb5c8625ae85d1b2b84b655540a2bf6502d
PatrickFK <38967356+PatrickFK@users.noreply.github.com>
2018-05-03 23:58:42 -0300

    Estrutura de menus

---
+++ loja.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<head>
+ <meta charset="utf-8"/>
+
+   <title>Loja</title>
+
+
+</head>
+
+
+
+<body>
+   <h1>Controle de estoque<h1>
+   
+   <h2>Compras<h2>
+   <h2>Venda<h2>
+   <h2>Estoque<h2>
+   <h2>perdas<h2>
+   
+   
+   
+   
+   <?php
+      echo "dividendos";
+   
+   
+   
+   ?>
+   
+</body>
+
+
+
+
+
+</html>
